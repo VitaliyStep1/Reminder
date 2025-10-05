0461454a6afffa7009cc8343191b3ac27ffd5122
Vitalii Stepanenko <SrbVitaliy@gmail.com>
2025-10-05 16:31:58 +0300

    Router was added

---
+++ Reminder/Navigation/Route.swift
@@ -0,0 +1,14 @@
+//
+//  Untitled.swift
+//  Reminder
+//
+//  Created by Vitaliy Stepanenko on 05.10.2025.
+//
+
+enum Route: Hashable {
+  case start
+  case splash
+  case categories
+  case category(ObjectId)
+  case closest
+}
---
+++ Reminder/Navigation/Router.swift
@@ -0,0 +1,29 @@
+//
+//  Router.swift
+//  Reminder
+//
+//  Created by Vitaliy Stepanenko on 05.10.2025.
+//
+
+import SwiftUI
+
+@MainActor
+final class Router: ObservableObject {
+  @Published var path = NavigationPath()
+  
+  func pushScreen(_ route: Route) {
+    path.append(route)
+  }
+  
+  func resetToScreen(_ route: Route) {
+    path = NavigationPath()
+    path.append(route)
+  }
+  
+  func popScreen() {
+    guard !path.isEmpty else {
+      return
+    }
+    path.removeLast()
+  }
+}
