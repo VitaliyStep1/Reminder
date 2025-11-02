7fe001bdb3a351dcab6bcaa21a24c872ccce7377
Vitalii Stepanenko <SrbVitaliy@gmail.com>
2025-11-02 23:57:28 +0200

    Refactoring - Tuist was added

---
+++ Project.swift
@@ -0,0 +1,79 @@
+import ProjectDescription
+
+let projectSettings: Settings = .settings(
+  base: [
+    "CODE_SIGN_STYLE": "Automatic",
+    "CURRENT_PROJECT_VERSION": "1",
+    "MARKETING_VERSION": "1.0",
+    "SWIFT_VERSION": "5.0",
+    "IPHONEOS_DEPLOYMENT_TARGET": "16.6"
+  ],
+  configurations: [
+    .debug(name: "Debug"),
+    .release(name: "Release")
+  ],
+  defaultSettings: .recommended
+)
+
+let appSettings: Settings = .settings(
+  base: [
+    "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
+    "ASSETCATALOG_COMPILER_GLOBAL_ACCENT_COLOR_NAME": "AccentColor",
+    "ENABLE_PREVIEWS": "YES",
+    "GENERATE_INFOPLIST_FILE": "YES",
+    "INFOPLIST_KEY_UIApplicationSceneManifest_Generation": "YES",
+    "INFOPLIST_KEY_UIApplicationSupportsIndirectInputEvents": "YES",
+    "INFOPLIST_KEY_UILaunchScreen_Generation": "YES",
+    "INFOPLIST_KEY_UISupportedInterfaceOrientations_iPad": "UIInterfaceOrientationPortrait UIInterfaceOrientationPortraitUpsideDown",
+    "INFOPLIST_KEY_UISupportedInterfaceOrientations_iPhone": "UIInterfaceOrientationPortrait",
+    "LD_RUNPATH_SEARCH_PATHS": ["$(inherited)", "@executable_path/Frameworks"],
+    "SWIFT_EMIT_LOC_STRINGS": "YES",
+    "TARGETED_DEVICE_FAMILY": "1,2"
+  ]
+)
+
+let project = Project(
+  name: "Reminder",
+  settings: projectSettings,
+  targets: [
+    .target(
+      name: "Reminder",
+      destinations: .iOS,
+      product: .app,
+      bundleId: "com.ojexsoft.Reminder",
+      infoPlist: .default,
+//      infoPlist: .extendingDefault(
+//        with: [
+//          "UILaunchScreen": [
+//            "UIColorName": "",
+//            "UIImageName": "",
+//          ],
+//        ]
+//      ),
+      buildableFolders: [
+        "Reminder/Sources",
+        "Reminder/Resources",
+      ],
+      dependencies: [
+        .external(name: "ReminderConfigurations"),
+        .external(name: "ReminderDependencies"),
+        .external(name: "ReminderDomain"),
+        .external(name: "ReminderDomainContracts"),
+        .external(name: "ReminderPersistence"),
+        .external(name: "ReminderPersistenceContracts"),
+        .external(name: "ReminderResolver"),
+        .external(name: "ReminderUserDefaultsStorage"),
+        .external(name: "ReminderClosestTab"),
+        .external(name: "ReminderCreateTab"),
+        .external(name: "ReminderMainTabView"),
+        .external(name: "ReminderMainTabViewContracts"),
+        .external(name: "ReminderNavigation"),
+        .external(name: "ReminderNavigationContracts"),
+        .external(name: "ReminderSettingsTab"),
+        .external(name: "ReminderSharedUI"),
+        .external(name: "ReminderStartUI")
+      ],
+      settings: appSettings
+    ),
+  ]
+)
